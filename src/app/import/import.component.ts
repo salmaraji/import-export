3d68f8ccb910094bc51ff4b5bae4e0e55c0fa0cf
Youssef Chakir <youssef.chakir@e-polytechnique.ma>
2024-07-17 11:37:09 +0100

    add styling using TailwindCss

--- src/app/import/import.component.ts
+++ src/app/import/import.component.ts
@@ -4,10 +4,13 @@ import * as XLSX from 'xlsx';
 @Component({
   selector: 'app-import',
   templateUrl: './import.component.html',
-  styleUrls: ['./import.component.css']
+  styleUrls: ['./import.component.css'],
 })
 export class ImportComponent {
-  @Output() dataImported = new EventEmitter<{ columns: string[], importedData: any[][] }>();
+  @Output() dataImported = new EventEmitter<{
+    columns: string[];
+    importedData: any[][];
+  }>();
 
   selectedFile: File | null = null;
   importedData: any[][] = [];
@@ -19,10 +22,12 @@ export class ImportComponent {
       const file = input.files[0];
       const extension = file.name.split('.').pop()?.toLowerCase();
 
-      if (extension === 'xlsx') {
+      if (extension === 'xls') {
         this.selectedFile = file;
       } else {
-        alert(`Le fichier ${file.name} a une extension invalide. Seuls les fichiers .xlsx sont acceptés.`);
+        alert(
+          `Le fichier ${file.name} a une extension invalide. Seuls les fichiers .xlsx sont acceptés.`
+        );
         input.value = '';
       }
     }
@@ -35,18 +40,25 @@ export class ImportComponent {
 
     const reader: FileReader = new FileReader();
     reader.onload = (e: ProgressEvent<FileReader>) => {
-      const tabstr: string | ArrayBuffer = e.target?.result as string | ArrayBuffer;
+      const tabstr: string | ArrayBuffer = e.target?.result as
+        | string
+        | ArrayBuffer;
       const wb: XLSX.WorkBook = XLSX.read(tabstr, { type: 'binary' });
       const wsname: string = wb.SheetNames[0];
       const ws: XLSX.WorkSheet = wb.Sheets[wsname];
-      this.importedData = XLSX.utils.sheet_to_json(ws, { header: 1 }) as any[][];
+      this.importedData = XLSX.utils.sheet_to_json(ws, {
+        header: 1,
+      }) as any[][];
 
       if (this.importedData.length > 0) {
         this.columns = this.importedData[0] as string[];
         this.importedData = this.importedData.slice(1);
 
         // Émettre les données importées vers le parent (ou vers ExportComponent directement si elles sont imbriquées)
-        this.dataImported.emit({ columns: this.columns, importedData: this.importedData });
+        this.dataImported.emit({
+          columns: this.columns,
+          importedData: this.importedData,
+        });
       }
     };
     reader.readAsBinaryString(this.selectedFile);
